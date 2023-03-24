5c6d62dc4562669a97b57083b84b9d79a3cde604
noxus55 <ryan.daly@ucdconnect.ie>
2023-03-24 15:04:41 +0000

    Added Decision Matrix to Computer Player

--- blackjack/src/main/java/blackjack/BlackjackComputerPlayer.java
+++ blackjack/src/main/java/blackjack/BlackjackComputerPlayer.java
@@ -18,11 +18,35 @@ import java.util.Random;
 
 public class BlackjackComputerPlayer extends BlackjackPlayer{ // error caused by abstract methods such as shouldSplit etc
     
-    
+    char[][] decisionMatrix;
 
     //constructor
     public BlackjackComputerPlayer(String name, int money){
         super(name, money);
+
+        decisionMatrix = new char[][]{
+                // Dealer's hand in range of values 2-A
+                {'E','E','E','E','E','E','E','E','E','E'}, //computer's hand = 1
+                {'E','E','E','E','E','E','E','E','E','E'}, //computer's hand = 2
+                {'E','E','E','E','E','E','E','E','E','E'}, //computer's hand = 3
+                {'H','H','H','H','H','H','H','H','H','H'}, //computer's hand = 4
+                {'H','H','H','H','H','H','H','H','H','H'}, //computer's hand = 5
+                {'H','H','H','H','H','H','H','H','H','H'}, //computer's hand = 6
+                {'H','H','H','H','H','H','H','H','H','H'}, //computer's hand = 7
+                {'H','H','H','H','H','H','H','H','H','H'}, //computer's hand = 8
+                {'H','D','D','D','D','H','H','H','H','H'}, //computer's hand = 9
+                {'D','D','D','D','D','D','D','D','H','H'}, //computer's hand = 10
+                {'D','D','D','D','D','D','D','D','D','D'}, //computer's hand = 11
+                {'H','H','S','S','S','H','H','H','H','H'}, //computer's hand = 12
+                {'S','S','S','S','S','H','H','H','H','H'}, //computer's hand = 13
+                {'S','S','S','S','S','H','H','H','H','H'}, //computer's hand = 14
+                {'S','S','S','S','S','H','H','H','H','H'}, //computer's hand = 15
+                {'S','S','S','S','S','H','H','H','H','H'}, //computer's hand = 16
+                {'S','S','S','S','S','S','S','S','S','S'}, //computer's hand = 17
+                {'S','S','S','S','S','S','S','S','S','S'}, //computer's hand = 18
+                {'S','S','S','S','S','S','S','S','S','S'}, //computer's hand = 19
+                {'S','S','S','S','S','S','S','S','S','S'}, //computer's hand = 20
+        };
     }
 
     //always false computer never splits
